e438f4f6734d242e0f19c728693ae126676b5b9a
Minoru Takeuchi <31735614+dora1998@users.noreply.github.com>
2019-12-11 10:44:35 +0900

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:prettier/recommended',
+    'prettier/vue',
+    'prettier/@typescript-eslint'
+  ],
+  plugins: [
+    'prettier',
+  ],
+  // add your custom rules here
+  rules: {
+  }
+}
